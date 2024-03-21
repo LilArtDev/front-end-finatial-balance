7b79063a42a184d34eeb7a0310ddedd88db1c0e5
Arthur Fernandez <contatoarthurfernandez@gmail.com>
2024-03-21 10:50:06 -0300

    feat: navbar layout implemented

---
+++ src/components/HighlightInfo/highlight-info.component.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface HighlightInfoProps {
+  label: string;
+  value: number;
+  currency: string;
+}
+
+export function HighlightInfo({ currency, label, value }: HighlightInfoProps) {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
